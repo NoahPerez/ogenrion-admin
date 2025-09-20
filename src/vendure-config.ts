8c71948a70889aed85014cd8cf5a992bf0ea16ca
Noah Perez <noahperez@Mac.home>
2025-09-20 13:55:49 +0200

    database testing 3

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -47,7 +47,7 @@ export const config: VendureConfig = {
         connectTimeoutMS: 10000,
         // See the README.md "Migrations" section for an explanation of
         // the `synchronize` and `migrations` options.
-        synchronize: false,  // Change this to true for new database
+        synchronize: true,  // Change this to true
         // Completely remove migrations for now
         logging: false,
         database: process.env.DB_NAME,
