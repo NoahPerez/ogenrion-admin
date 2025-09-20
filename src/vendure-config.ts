0db3afd33ceb56c51f823ccf2efc165e76889158
Noah Perez <noahperez@Mac.home>
2025-09-20 13:36:33 +0200

    database

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -43,19 +43,19 @@ export const config: VendureConfig = {
         },
     },
     dbConnectionOptions: {
-       type: "postgres",
-    connectTimeoutMS: 10000,
-    // See the README.md "Migrations" section for an explanation of
-    // the `synchronize` and `migrations` options.
-    synchronize: true,
-    migrations: [path.join(__dirname, "./migrations/*.+(js|ts)")],
-    logging: false,
-    database: process.env.DB_NAME,
-    schema: process.env.DB_SCHEMA,
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+        type: 'postgres',
+        connectTimeoutMS: 10000,
+        // See the README.md "Migrations" section for an explanation of
+        // the `synchronize` and `migrations` options.
+        synchronize: true,
+        // Completely remove migrations for now
+        logging: false,
+        database: process.env.DB_NAME,
+        schema: process.env.DB_SCHEMA,
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
     },
     paymentOptions: {
         paymentMethodHandlers: [dummyPaymentHandler],
